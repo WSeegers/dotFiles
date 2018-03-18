21c691cde1c7638c6fb83616215e561f1f9206e8
Werner Seegers <wseegers.mauws@gmail.com>
2018-03-18 18:30:48 +0200

    added initial dotfiles

---
+++ .make.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# .make.sh
+# This scrip will create back up old dot files and create symlinks to new dot files
+
+dir=~/dotfiles
+budir=~/backup_dotfiles
+files=".zshrc .vimrc"
+
+echo -n "Creating $budir"
+mkdir -p $budir
+echo "...completed"
+
+echo -n "Changing to $dir directory"
+cd $dir
+echo "...completed"
+
+for file in $files; do
+	echo "Moving $file to $budir"
+	mv ~/$file $budir/$file
+	echo "Creating link to new $file"
+	ln -s $dir/$file ~/$file
+done
